f99163022005040feb6e0884c8b501c3700907f6
WUYUEZONG <w.yzong@outlook.com>
2022-06-10 10:32:27 +0800

    1

--- Sources/WZBaseComponents/UIViewExtensions.swift
+++ Sources/WZBaseComponents/UIViewExtensions.swift
@@ -8,15 +8,15 @@
 import UIKit
 
 
-public extension UIView {
-    
-    static func initNib(_ name: String? = nil, owner: Any?) -> Self? {
-        let nibName = name == nil ? Self.description() : name!
-        return Bundle.module.loadNibNamed(nibName, owner: owner, options: nil)?.first as? Self
-    }
-    
-    
-}
+//public extension UIView {
+//    
+//    static func initNib(_ name: String? = nil, owner: Any?) -> Self? {
+//        let nibName = name == nil ? Self.description() : name!
+//        return Bundle.module.loadNibNamed(nibName, owner: owner, options: nil)?.first as? Self
+//    }
+//    
+//    
+//}
 
 /// 简化坐标
 public extension UIView {
